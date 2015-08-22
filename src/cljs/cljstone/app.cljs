dde39c07f4b0f9cd431bdfa5de9b6051c6a6ced9
jrheard <jrheard@cs.stanford.edu>
2015-08-22 16:35:20 -0700

    tooling around

--- src/cljs/cljstone/app.cljs
+++ src/cljs/cljstone/app.cljs
@@ -1,3 +1,13 @@
 (ns cljstone.app)
 
-(.write js/document "<p>Hello, world!</p>")
+(defn get-nodes-by-selector [selector]
+  (.querySelectorAll js/document selector))
+
+(defn set-text! [node content]
+  (set! (. node -textContent) content))
+
+(def p1 (aget (get-nodes-by-selector "#board-half-1") 0))
+(def p2 (aget (get-nodes-by-selector "#board-half-2") 0))
+
+(set-text! p1 "sup")
+(set-text! p2 "yo")

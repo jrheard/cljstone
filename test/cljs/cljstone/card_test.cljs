c3bcf4cacd6715b9ec11a9080482098dda2e98d8
jrheard <jrheard@cs.stanford.edu>
2015-09-04 19:44:07 -0700

    turn on schema validation in card-test - verified that it actually does what it says. works great. this is awesome.

--- test/cljs/cljstone/card_test.cljs
+++ test/cljs/cljstone/card_test.cljs
@@ -1,6 +1,9 @@
 (ns cljstone.card-test
-  (:require [cljs.test :refer-macros [deftest is testing]])
-  (:use [cljstone.card :only [make-random-deck NUM-CARDS-IN-DECK]]))
+  (:require [cljs.test :refer-macros [deftest is testing use-fixtures]])
+  (:use [cljstone.card :only [make-random-deck NUM-CARDS-IN-DECK]]
+        [schema.test :only [validate-schemas]]))
+
+(use-fixtures :once validate-schemas)
 
 (deftest test-numbers
   (let [deck (make-random-deck)]

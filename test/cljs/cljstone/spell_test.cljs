6b135e3db843149f2f2745155a8db41ba0cdd527
jrheard <jrheard@cs.stanford.edu>
2015-09-11 10:25:18 -0700

    test that damage spells get combat logged

--- test/cljs/cljstone/spell_test.cljs
+++ test/cljs/cljstone/spell_test.cljs
@@ -2,6 +2,7 @@
   (:require [cljs.test :refer-macros [deftest testing is use-fixtures]]
             [cljstone.spell :as s])
   (:use [schema.test :only [validate-schemas]]
+        [cljstone.combat-log :only [get-next-log-entry-id]]
         [cljstone.test-helpers :only [three-minions-per-player-board]]
         [cljstone.board :only [make-board play-card]]))
 
@@ -11,7 +12,12 @@
   (let [board (assoc-in three-minions-per-player-board
                         [:player-1 :hand 0]
                         (s/spell->card (s/all-spells :flamecannon)))]
-    (with-redefs [rand-nth first]
-      (let [board (play-card board :player-1 0)]
+    (with-redefs [rand-nth first
+                  get-next-log-entry-id (fn [] 0)]
+      (let [minion (get-in board [:player-2 :minions 0])
+            board (play-card board :player-1 0)]
         (is (= (get-in board [:player-2 :minions 0 :modifiers])
-               [{:type :damage-spell :name "Flamecannon" :effect {:health -4}}]))))))
+               [{:type :damage-spell :name "Flamecannon" :effect {:health -4}}]))
+
+        (is (= (-> board :combat-log first)
+               {:modifier {:type :damage-spell :name "Flamecannon" :effect {:health -4}} :id 0 :source nil :target minion}))))))

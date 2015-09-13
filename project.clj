019ea4ff06d4dda0b2575865a9b44cfc8bcc8297
jrheard <jrheard@cs.stanford.edu>
2015-09-13 12:37:54 -0700

    add core.async dependency

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.48"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [prismatic/schema "1.0.1"]
                  [reagent "0.5.1-rc"]]
   :plugins [[lein-cljsbuild "1.0.6"]

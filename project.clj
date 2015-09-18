12a72de49ac5365c909bb7bcc50c320bcdd6692e
jrheard <jrheard@cs.stanford.edu>
2015-09-17 22:06:27 -0700

    set up figwheel

--- project.clj
+++ project.clj
@@ -9,21 +9,24 @@
                  [prismatic/schema "1.0.1"]
                  [reagent "0.5.1-rc"]]
   :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-doo "0.1.5-SNAPSHOT"]]
+            [lein-doo "0.1.5-SNAPSHOT"]
+            [lein-figwheel "0.4.0"]]
   :cljsbuild
     {:builds
       {:dev {:compiler
-               {:output-dir "resources/public/js"
-                :output-to "resources/public/js/core.js"
-                :optimizations :whitespace
+               {:output-dir "resources/public/js/out"
+                ;:optimizations :whitespace
                 ;:source-map "resources/public/js/core.js.map"
-                :pretty-print true}
-             :source-paths ["src/cljs"]}
+                ;:pretty-print true
+                :output-to "resources/public/js/core.js"}
+             :source-paths ["src/cljs"]
+             :figwheel true}
        :test {:compiler
-              {:main 'cljstone.runner
+              {:main "cljstone.runner"
                :output-to "test_resources/test.js"
                :optimizations :whitespace
                :pretty-print true}
               :source-paths ["src/cljs" "test/cljs"]}}}
+  :figwheel { :css-dirs ["resources/public/css"] }
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})

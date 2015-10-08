f8078da63f6b60a5ec7943ee17f51ab39c5aad3a
jrheard <jrheard@cs.stanford.edu>
2015-10-08 09:17:32 -0700

    playing around with scraper - turns out that a surprisingly small number of cards don't have text, like 22 including weapons

--- src/clj/tools/card_scraper.clj
+++ src/clj/tools/card_scraper.clj
@@ -2,10 +2,35 @@
   (:require [cheshire.core :refer [parse-string]]))
 
 
-(defonce contents (parse-string (slurp "http://hearthstonejson.com/json/AllSets.json") true))
+(defonce contents (-> "http://hearthstonejson.com/json/AllSets.json"
+                      slurp
+                      (parse-string true)))
+
+(defn parse-card [card]
+  (select-keys card [:faction :name :type :mechanics :health :attack :cost :text :playerClass]))
+
+(defn get-card-set [card-set-kw]
+  (->> contents
+       card-set-kw
+       (filter :collectible)
+       (filter #(not= (:type %) "Hero"))
+       (map parse-card)))
+
+(def basic (get-card-set :Basic))
+
+(def vanilla-basic (filter #(not (contains? % :text)) basic))
+
+(def basic-minions (filter #(= (:type %) "Minion") basic))
+
 
 (comment
   (prn (keys contents))
+
+  (count basic)
+
+  (take 10 (drop 10 basic))
+
+  (prn basic-minions)
 
   )
 
